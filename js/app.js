d66f71076222eef5ba04de1abe3f5d2bc4fb2c92
unknown <yunjie_miao@163.com>
2018-03-28 19:58:53 +0800

    second: change some knockout methods

--- js/app.js
+++ js/app.js
@@ -1,114 +1,116 @@
-var data_info = [	//地图标点基础数据
-	[121.510825,31.303319,"复旦大学"],	
-	[121.508186,31.289124,"同济大学"],
-	[121.431398,31.149172,"华东理工大学"],
-	[121.49713,31.244559,"外滩情人墙"],
-	[121.406081,31.230879,"长风公园"],
-	[121.498981,31.232772,"豫园"]
+//地图标点基础数据
+var data_info = [	
+		{
+			lng:121.510825,
+			lat:31.303319,
+			name:"复旦大学"
+		},
+		{	
+			lng:121.508186,
+			lat:31.289124,
+			name:"同济大学"
+		},
+		{
+			lng:121.431398,
+			lat:31.149172,
+			name:"华东理工大学"
+		},
+		{
+			lng:121.49713,
+			lat:31.244559,
+			name:"外滩情人墙"
+		},
+		{
+			lng:121.406081,
+			lat:31.230879,
+			name:"长风公园"
+		},
+		{	
+			lng:121.498981,
+			lat:31.232772,
+			name:"豫园"
+		}
 ];
-var opts = {	//信息窗口基础数据
-	width : 500,     // 信息窗口宽度
-	height: 160,     // 信息窗口高度
-	title : "<h5>介绍：</h5>" , // 信息窗口标题
+//信息窗口基础数据
+var opts = {	
+	width : 500,     
+	height: 160,     
+	title : "<h5>介绍：</h5>" , 
 	enableAutoPan : true,
 	maxWidth : 730
 };
 
 var ViewModel = function(){
-	var map = new BMap.Map('map');	//创建地图
+	var self = this;
+	//创建百度地图
+	var map = new BMap.Map('map');	
 	var point = new BMap.Point(121.467603,31.235558);
-	map.centerAndZoom(point, 13);
+	map.centerAndZoom(point, 12);
 	map.addControl(new BMap.MapTypeControl({
 		mapTypes:[
 	        BMAP_NORMAL_MAP,
 	        BMAP_HYBRID_MAP
 	    ]}));	  
-	map.setCurrentCity("上海");		//设置城市名称     
-	map.enableScrollWheelZoom(true); 	//设置鼠标滚动缩放地图
-	createPoints(data_info);
+	map.setCurrentCity("上海");	   
+	map.enableScrollWheelZoom(true); 	
+	createPoints(data_info);	
 	//筛选输入框，默认字符消失
+	this.textInput = ko.observable("");
 	this.inputClick = function(){
-		var gjz = $("#suggestId")[0].value;
-		if (gjz !== "") {
-			$("#suggestId")[0].value = "";
+		if(this.textInput() !== ""){
+			this.textInput('');
 		}
 	}
 	//隐藏、显示筛选框
+	this.changeList = ko.observable(false);
 	this.clickImage = function(){
-		
-		if ($("#list").css("display") === "block"){
-			$("#list").css("display","none");
-			$("#right").css("width","100%");
+		if(this.changeList() === true){
+			this.changeList(false);
 		} else {
-			$("#list").css("display","block");
-			$("#right").css("width","calc(100% - 175px)");
+			this.changeList(true);
 		}
-		
 	}
-	//列表筛选功能
+	//列表
+	this.dataList = ko.observable(data_info);
+	//列表点击事件
+	this.listHandler = function(data){
+		var points = new BMap.Point(data.lng,data.lat);
+		var marker = new BMap.Marker(points);             
+		var content = data.name;
+		getAPI(marker,content,points);
+	}
+	//筛选功能
 	this.clickSubmit = function(){
-		var gjz = $("#suggestId")[0].value;
-		var xianshi = [];
-		if (gjz == ""){
-			$("ul")[0].innerHTML = "";
+		if (this.textInput() == ""){
+			this.dataList(data_info);
 			createPoints(data_info);
-			$("ul li").show();
 		} else {
-			$("ul li").each(function(){
-				var name = this.innerText;
-				map.clearOverlays();
-				if( name.indexOf(gjz) != -1){
-					$(this).show();
-					xianshi.push(name);
-				} else {
-					$(this).hide();
-				}
+			var newLists = ko.utils.arrayFilter(data_info, function(el, index) {
+			    return el.name.indexOf(self.textInput()) != -1;
 			});
-		}
-		for(var i=0;i<data_info.length;i++){	//修改地图显示图标
-			if (xianshi.indexOf(data_info[i][2]) != -1){
-				var points = new BMap.Point(data_info[i][0],data_info[i][1]);
-				var marker = new BMap.Marker(points);  // 创建标注
-				var content = data_info[i][2];		//设置标点信息窗口描述数据
-				map.addOverlay(marker);               // 将标注添加到地图中
-				addClickHandler(content,marker);	//将窗口信息添加到标注中
-			}	
+			this.dataList(newLists);
+			createPoints(newLists);
 		}
 	}
-	//创建列表
-	function createList(e){
-		var a = document.createElement('li');
-		a.innerHTML = e;
-		$('ul')[0].appendChild(a);
-	}
-	//创建标注及列表
+	//创建标注
 	function createPoints(datas){
-		for(var i=0; i < datas.length; i++){
-			var points = new BMap.Point(datas[i][0],datas[i][1]);
-			var marker = new BMap.Marker(points);  // 创建标注
-			map.addOverlay(marker);             // 将标注添加到地图中
-			var content = datas[i][2];	
+		//初始化标注
+		map.clearOverlays();	
+		datas.forEach(function(data){
+			var points = new BMap.Point(data.lng,data.lat);
+			var marker = new BMap.Marker(points);  
+			map.addOverlay(marker);            
+			var content = data.name;	
 			addClickHandler(content,marker);
-			//由于
-			var listKey = [datas[i][2]];
-			createList(listKey);				//创建标注点列表
-			$("li")[i].addEventListener("click",(function(marker,content,points){
-				return function(){
-					getAPI(marker,content,points);
-				}
-			})(marker,content,points));
-		}
+		});
 	}
-	//标记点击响应事件
+	//标注点击事件
 	function addClickHandler(content,marker){
 		marker.addEventListener("click",function(e){
-			openInfo(marker,content,e)}
-		);
-	}
-	function openInfo(marker,content,e){
-		var p = e.target;
-		var point = new BMap.Point(p.getPosition().lng, p.getPosition().lat);
-		getAPI(marker,content,point);
+			var p = e.target;
+			var point = new BMap.Point(p.getPosition().lng, p.getPosition().lat);
+			getAPI(marker,content,point);
+		});
 	}
 	//获取第三方api数据
 	function getAPI(marker,content,point){
@@ -134,5 +136,4 @@ var ViewModel = function(){
 	    });
 	}
 }
-
 ko.applyBindings(new ViewModel());
